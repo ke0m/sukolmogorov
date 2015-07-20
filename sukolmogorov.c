2eb5fce4f2612c274635660006519fe070b23652
Joseph Jennings <joseph.s.jennings@gmail.com>
2015-07-19 18:19:57 -0700

    Has a working shift if we want it

--- sukolmogorov.c
+++ sukolmogorov.c
@@ -21,21 +21,20 @@ char *sdoc[] = {
   "									     ",
   NULL};
 
-/* Credits:
+/* Credits: Based on Sergey Fomel's sfkolmog
  *	SEP: 
  *	CWP: 
  *	TCCS: 
- * Technical Reference:
+ * Technical Reference: TODO: a geophysics style biblio for GIEE.
+ *  
  *
- * Trace header fields accessed: ns,
+ * Trace header fields accessed: ns, trid, d1
  */
 
 /*
  * TODO:
- * 1. Put in the cosine taper window
- * 2. Take in a time domain (or other type of signal)
- *    a. With this, make sure that the sample rates and lengths are the same
  * 3. Test code on 1,-0.9 and -0.9,1 wavelets
+ * 4. Try to implement an optional shift in this code
  */
 
 /**************** end self doc *******************************************/
@@ -59,6 +58,7 @@ main(int argc, char **argv)
   int specsq;       /* Check if the input is the spectrum or spectrum squared */
   int verbose;      /* flag to get advisory messages */
   int lag;          /* lag for asymmetric part */
+  int shift;
   float asym;       /*(?) For applying the cosine window to the trace */
   float newd1;      /* New sampling interval for amplitude spectra input */
   cwp_Bool seismic; /* Check if the input is seismic data */
@@ -73,6 +73,7 @@ main(int argc, char **argv)
 
   /* Getting and setting parameters */
   if (!getparint("lag"    ,  &lag    ))    lag     = 0;
+  if (!getparint("shift"  ,  &shift  ))    shift   = 0;
   if (!getparint("log"    ,  &logar  ))    logar   = 0;
   if (!getparint("specsq" ,  &specsq ))    specsq  = 0;
   if (!getparint("verbose",  &verbose))    verbose = 0;
@@ -105,7 +106,6 @@ main(int argc, char **argv)
     for(i = 0; i < nf; i++) {
       rt[i] = (ct[i].r * ct[i].r) + (ct[i].i * ct[i].i);
     }
-    //fprintf(stderr, "nt=%d nfft=%d nf=%d \n", nt,nfft,nf);
   }
   else if(tr.trid == AMP_SPEC) {
     //TODO: How do I know what is the correct nfft?
@@ -120,12 +120,12 @@ main(int argc, char **argv)
 
     /* Computing sampling interval */
     if(tr.d1) {
-      newd1 = 1/(nfft*tr.d1);
+      newd1 = 1/(nfft*tr.d1); //TODO:I am not sure if this is correct...
     }
     else {
       if(tr.dt) newd1 = (float) (((double) tr.dt)/1000000.0);
       else {
-        warn("You did not specify a d1 or dt. Setting to 1...");
+        warn("You did not specify a d1 or dt. Setting d1 to 1...");
         newd1 = 1.f;
       }
     }
@@ -142,23 +142,16 @@ main(int argc, char **argv)
         rt[i] *= rt[i];
       }
     }
-    //fprintf(stderr, "nt=%d nfft=%d nf=%d \n", nt,nfft,nf);
   }
   else {
     err("I do not know how to work with the data you provided. \
         I can only work on time series or amplitude spectra");
   }
 
-  //fprintf(stderr, "input spec: \n");
-  //for(i = 0; i < nfft; i++) {
-  //  fprintf(stderr, "i=%d input=%f\n",i,rt[i]);
-  //}
-  //fprintf(stderr, "\n");
-
   /* Take the log and create a complex type */
   if(!logar) {
-    for(i = 0; i < nf; i++) { //This needs to be the length of the data
-      ct[i].r = log(rt[i]+eps)/nfft; // Therefore, the nfft needs to be twice that length
+    for(i = 0; i < nf; i++) {
+      ct[i].r = log(rt[i]+eps)/nfft;
       ct[i].i = 0.f;
     }
   }
@@ -169,29 +162,16 @@ main(int argc, char **argv)
     }
   }
 
-  //fprintf(stderr, "log of spec(nfft=%d): \n",nfft);
-  //for(i = 0; i < nf; i++) {
-  //  fprintf(stderr, "i=%d real=%f imag=%f\n",i,ct[i].r,ct[i].i);
-  //}
-  //fprintf(stderr, "\n");
-
   /* Find the inverse FFT */
-  //fprintf(stderr, "the ifft: \n");
   pfacr(-1, nfft, ct, rt);
-  /*
-     for(i = 0; i < nfft; i++) {
-     fprintf(stderr, "i=%d ifft=%f\n",i,tr.data[i]);
-     }
-     fprintf(stderr, "\n");
-     */
-
+  
   rt[0]      *= 0.5;
   rt[nfft/2] *= 0.5;
   for(i=1+nfft/2; i < nfft; i++) {
     rt[i] = 0.;
   }
 
-  // Put in cosine taper window
+  //TODO: I am really not sure how to use this...
   for (i=1; i < lag; i++) {
     asym = cosf(0.5*PI*i/(lag-1.0));  /* tapering weight */
     asym *= 0.5*rt[i]*asym; 
@@ -201,27 +181,31 @@ main(int argc, char **argv)
 
   pfarc(1,nfft,rt,ct);
 
+  //TODO: Perhaps change this to real and imaginary parts
   for(int i=0; i < nf; i++) {
     ct[i] = crmul(cwp_cexp(ct[i]),1./nfft);
   }
 
-  //fprintf(stderr, "output: \n");
+  if (0 != shift) {
+    for (i=0; i < nf; i++) {
+      ct[i] = cmul(ct[i],cwp_cexp(cmplx(0.0,2*M_PI*i*shift/nfft)));
+    }
+  }   
+  
+
   pfacr(-1,nfft,ct,rt);
 
   /* Copy data back to tr */
   if(seismic) {
-    for(i = 0; i < nt; i++) { // I am not sure but this might cause problems for the time domain case???
+    for(i = 0; i < nt; i++) {
       tr.data[i] = rt[i];
     }
   }
   else {
-    //TODO: What other fields should I set here:
-    //      d1, dt, ns?
     if(tr.trid == AMP_SPEC) {
       tr.trid = TREAL;
       tr.ns = nfft;
       tr.d1 = newd1;
-      //tr.dt = 0.004; // for testing...
       if(!tr.f1) {
         tr.f1 = 0.f;
       }
@@ -230,7 +214,6 @@ main(int argc, char **argv)
       /* Copy back to tr */
       for(i = 0; i < nfft; i++) {
         tr.data[i] = rt[i];
-        //fprintf(stderr, "i=%d t=%f output=%f\n",i,o1+i*dt,rt[i]);
       }
     }
     else {
